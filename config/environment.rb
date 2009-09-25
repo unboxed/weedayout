46e8147cc73778b0128cd6fd936871301403e19d
Ben Wong <ben.wong@unboxedconsulting.com>
2009-09-25 17:04:22 +0100

    added cucumber 0.3.103

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "geokit"
+  config.gem "cucumber", :lib => false, :version => ">= 0.3.103"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
