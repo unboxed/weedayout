f657fdfd64ac28c8ac5331bd058e51b23c82b439
Ben Wong <ben.wong@unboxedconsulting.com>
2009-10-02 12:43:57 +0100

    refactoring cucumber steps

---
+++ features/step_definitions/project_steps.rb
@@ -0,0 +1,21 @@
+Then /^I should see the following text on the page:$/ do |text_table|
+  text_table.hashes.each do |hash|
+    response.should contain(hash[:text])
+  end
+end
+
+Then /^I should see the following elements with content:$/ do |element_table|
+  element_table.hashes.each do |hash|
+    within(hash["element"]) do |content|
+      content.should contain(hash["content"])
+    end
+  end
+end
+
+Then /^my url should end with "(.*)"$/ do |expected_suffix|
+  len = expected_suffix.length
+  url = URI.parse(current_url).path
+  actual_suffix = url[url.length - len, url.length - 1]
+  actual_suffix.should == expected_suffix
+end
+
