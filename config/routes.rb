382337e2de1c97996069e195d66daac078e6e5e2
Ben Wong <ben.wong@unboxedconsulting.com>
2009-10-04 20:57:10 +0100

    more sensible action name

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :toilets
   map.root :toilets
 
-  map.connect 'toilets/:permalink', :controller => 'toilets', :action => 'view'
+  map.connect 'toilets/:permalink', :controller => 'toilets', :action => 'show'
 end
