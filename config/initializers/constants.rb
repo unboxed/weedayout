c8bd0b1b77aa5f86396a073e0146146de67f9b20
Attila Györffy <attila.gyorffy@unboxedconsulting.com>
2010-04-14 13:52:45 +0100

    Add GMaps AI keys for staging env.

---
+++ config/initializers/constants.rb
@@ -0,0 +1,6 @@
+GOOGLE_MAPS_API_KEYS = {
+  :development  => 'ABQIAAAAlyeJljQH98VMTkAORPPxhxRi_j0U6kJrkFvY4-OX2XYmEAa76BR1dkIiZ5uJBrN49TH92bIh5AJW7w',
+  :staging      => 'ABQIAAAAtKHuZgZ6ZYmkr7zOvb3BEBQZuDB81lYR9ehagyKbdtoSfbk_cxSfrlO1nBnD40vVsqVh48IztGmTJw',
+  :cucumber     => '',
+  :production   => ''
+}
