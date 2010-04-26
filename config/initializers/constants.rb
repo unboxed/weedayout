6df73e98a1819a35b94e619dfe0f451bf5a774bf
Attila Györffy <attila.gyorffy@gmail.com>
2010-04-26 13:05:28 +0100

    Change the staging url

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 GOOGLE_MAPS_API_KEYS = {
   'localhost'                             => 'ABQIAAAAlyeJljQH98VMTkAORPPxhxRi_j0U6kJrkFvY4-OX2XYmEAa76BR1dkIiZ5uJBrN49TH92bIh5AJW7w',
-  'weedayout-stage.unboxedconsulting.com' => 'ABQIAAAAtKHuZgZ6ZYmkr7zOvb3BEBQZuDB81lYR9ehagyKbdtoSfbk_cxSfrlO1nBnD40vVsqVh48IztGmTJw',
+  'weedayout.staging.unboxedconsulting.com' => 'ABQIAAAAtKHuZgZ6ZYmkr7zOvb3BEBQZuDB81lYR9ehagyKbdtoSfbk_cxSfrlO1nBnD40vVsqVh48IztGmTJw',
 }
