047707bf90c85c19e8e489fb8e543c6a70640d08
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2010-12-21 10:10:15 +0000

    New maps api keys for weedayout.heroku.com and weeedayout.com

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,6 @@
 GOOGLE_MAPS_API_KEYS = {
   'localhost'                             => 'ABQIAAAAlyeJljQH98VMTkAORPPxhxRi_j0U6kJrkFvY4-OX2XYmEAa76BR1dkIiZ5uJBrN49TH92bIh5AJW7w',
   'weedayout.staging.unboxedconsulting.com' => 'ABQIAAAAtKHuZgZ6ZYmkr7zOvb3BEBQZuDB81lYR9ehagyKbdtoSfbk_cxSfrlO1nBnD40vVsqVh48IztGmTJw',
+  'weedayout.com' => 'ABQIAAAAA32jREhTKTPKAunStcB2VRTxuOKrlX951rpGrLdiCOiPsdQ-tRQIUjPzbccJ67InFrmS-d4g5xgn8A',
+  'weedayout.heroku.com' => 'ABQIAAAAA32jREhTKTPKAunStcB2VRSAfsbZZTq08ccjMZMPrvX_PingAhSaT_9KN7m9ZDWSMmkaGNNsgT31UA'
 }
