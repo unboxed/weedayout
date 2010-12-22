d04758223dc12bda047d3a56aa0c51b34cfad402
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2010-12-22 16:37:16 +0000

    New api key for new staging on heroku

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,5 +2,6 @@ GOOGLE_MAPS_API_KEYS = {
   'localhost'                             => 'ABQIAAAAlyeJljQH98VMTkAORPPxhxRi_j0U6kJrkFvY4-OX2XYmEAa76BR1dkIiZ5uJBrN49TH92bIh5AJW7w',
   'weedayout.staging.unboxedconsulting.com' => 'ABQIAAAAtKHuZgZ6ZYmkr7zOvb3BEBQZuDB81lYR9ehagyKbdtoSfbk_cxSfrlO1nBnD40vVsqVh48IztGmTJw',
   'weedayout.com' => 'ABQIAAAAA32jREhTKTPKAunStcB2VRTxuOKrlX951rpGrLdiCOiPsdQ-tRQIUjPzbccJ67InFrmS-d4g5xgn8A',
+  'weedayout-staging.heroku.com' => 'ABQIAAAAA32jREhTKTPKAunStcB2VRRvkCQBkn2TBhxG6E8PyngI8L6vdxRqAw0_0cBg075bsg-PV_YYEKb_TQ',
   'weedayout.heroku.com' => 'ABQIAAAAA32jREhTKTPKAunStcB2VRSAfsbZZTq08ccjMZMPrvX_PingAhSaT_9KN7m9ZDWSMmkaGNNsgT31UA'
 }
